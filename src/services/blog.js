3e68b34fa81bc474ca1415b53717b5953410bddc
Nandan-unni <asnqln@gmail.com>
2021-07-05 15:41:04 +0530

    fixed: new API

--- src/services/blog.js
+++ src/services/blog.js
@@ -1,52 +1,49 @@
-import axios from 'axios';
+import axios from "axios";
 // const API = "http://localhost:8000/api";
-const API = "https://keyblogsapi.herokuapp.com/api";
-
+const API = "https://blogbookapi.herokuapp.com/api";
 
 export const createBlogAPI = async (data) => {
-    const url = `${API}/blog/create/`;
-    var response;
-    try {
-        response = await axios.post(url, data);
-        console.log(response.status);
-    } catch(error) {
-        console.log(error);
-    };
-    return response.status;
+  const url = `${API}/blog/create/`;
+  var response;
+  try {
+    response = await axios.post(url, data);
+    console.log(response.status);
+  } catch (error) {
+    console.log(error);
+  }
+  return response.status;
 };
-
 
 export const getBlogAPI = async (pk) => {
-    const url = `${API}/blog/manage/${pk}/`;
-    return axios.get(url).then(response => response.data);
+  const url = `${API}/blog/manage/${pk}/`;
+  return axios.get(url).then((response) => response.data);
 };
-
 
 export const updateBlogAPI = async (pk, data) => {
-    const url = `${API}/blog/manage/${pk}/`;
-    return axios.patch(url, data).then(response => {return response;})
+  const url = `${API}/blog/manage/${pk}/`;
+  return axios.patch(url, data).then((response) => {
+    return response;
+  });
 };
-
 
 export const deleteBlogAPI = async (pk) => {
-    const url = `${API}/blog/manage/${pk}/`;
-    return axios.delete(url).then(response => {return response;})
+  const url = `${API}/blog/manage/${pk}/`;
+  return axios.delete(url).then((response) => {
+    return response;
+  });
 };
-
 
 export const likeBlogAPI = async (user_pk, blog_pk) => {
-    const url = `${API}/blog/like/${blog_pk}/${user_pk}/`;
-    return axios.get(url);
+  const url = `${API}/blog/like/${blog_pk}/${user_pk}/`;
+  return axios.get(url);
 };
-
 
 export const saveBlogAPI = async (user_pk, blog_pk) => {
-    const url = `${API}/blog/save/${blog_pk}/${user_pk}/`;
-    return axios.get(url);
+  const url = `${API}/blog/save/${blog_pk}/${user_pk}/`;
+  return axios.get(url);
 };
 
-
 export const feedAPI = async () => {
-    const url = `${API}/blog/feed/`;
-    return axios.get(url).then(response => response.data);
+  const url = `${API}/blog/feed/`;
+  return axios.get(url).then((response) => response.data);
 };
--- src/services/writer.js
+++ src/services/writer.js
@@ -1,77 +1,77 @@
-import axios from 'axios';
+import axios from "axios";
 // const API = "http://localhost:8000/api";
-const API = "https://keyblogsapi.herokuapp.com/api";
-
+const API = "https://blogbookapi.herokuapp.com/api";
 
 export const loginWriterAPI = async (data) => {
-    const url = `${API}/writer/login/`;
-    var response;
-    try {
-        response = await axios.post(url, data);
-        console.log(response.status);
-    } catch(error) {
-        console.log(error);
-    };
-    return response;
+  const url = `${API}/writer/login/`;
+  var response;
+  try {
+    response = await axios.post(url, data);
+    console.log(response.status);
+  } catch (error) {
+    console.log(error);
+  }
+  return response;
 };
-
 
 export const logoutWriterAPI = async (pk) => {
-    const url = `${API}/writer/logout/${pk}/`;
-    axios.get(url);
-}
-
+  const url = `${API}/writer/logout/${pk}/`;
+  axios.get(url);
+};
 
 export const createWriterAPI = async (data) => {
-    const url = `${API}/writer/create/`;
-    return axios.post(url, data).then(response => { return response })
+  const url = `${API}/writer/create/`;
+  return axios.post(url, data).then((response) => {
+    return response;
+  });
 };
-
 
 export const setupWriterAPI = async (pk, data) => {
-    const url = `${API}/writer/setup/${pk}/`;
-    return axios.post(url, data).then(response => { return response })
-}
-
+  const url = `${API}/writer/setup/${pk}/`;
+  return axios.post(url, data).then((response) => {
+    return response;
+  });
+};
 
 export const getWriterAPI = async (pname) => {
-    const url = `${API}/writer/manage/${pname}/`;
-    return axios.get(url).then(response => response.data);
+  const url = `${API}/writer/manage/${pname}/`;
+  return axios.get(url).then((response) => response.data);
 };
-
 
 export const updateWriterAPI = async (pname, data) => {
-    const url = `${API}/writer/manage/${pname}/`;
-    return axios.patch(url, data).then(response => {return response;})
+  const url = `${API}/writer/manage/${pname}/`;
+  return axios.patch(url, data).then((response) => {
+    return response;
+  });
 };
-
 
 export const deleteWriterAPI = async (pname, data) => {
-    const url = `${API}/writer/delete/${pname}/`;
-    var response;
-    try {
-        response = await axios.post(url, data);
-        console.log(response.status);
-    } catch(error) {
-        console.log(error);
-    };
-    return response;
+  const url = `${API}/writer/delete/${pname}/`;
+  var response;
+  try {
+    response = await axios.post(url, data);
+    console.log(response.status);
+  } catch (error) {
+    console.log(error);
+  }
+  return response;
 };
-
 
 export const getWritersAPI = async () => {
-    const url = `${API}/writer/search/`;
-    return axios.get(url).then(response => response.data);
+  const url = `${API}/writer/search/`;
+  return axios.get(url).then((response) => response.data);
 };
-
 
 export const followWriterAPI = async (user_pk, writer_pk) => {
-    const url = `${API}/writer/follow/${user_pk}/${writer_pk}/`;
-    return axios.get(url).then(response => {return response;})
+  const url = `${API}/writer/follow/${user_pk}/${writer_pk}/`;
+  return axios.get(url).then((response) => {
+    return response;
+  });
 };
 
-
 export const searchWriterAPI = async (data) => {
-    const url = `${API}/writer/search/`;
-    return axios.post(url, data).then(response => { return response })
+  const url = `${API}/writer/search/`;
+  return axios.post(url, data).then((response) => {
+    return response;
+  });
 };
