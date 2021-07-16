ffe2a2965c4d9bdfa5d9f5d83c22b72c8e828ab0
Nandan-unni <asnqln@gmail.com>
2021-07-16 17:15:25 +0530

    removed old

--- src/app_old/components/index.js
+++
@@ -1,9 +0,0 @@
-import AccountCard from "./AccountCard";
-import Button from "./Button";
-import Footer from "./Footer";
-import Logo from "./Logo";
-import Navbar from "./Navbar";
-import Portal from "./Portal";
-import Stud from "./Stud";
-
-export { AccountCard, Button, Footer, Logo, Navbar, Portal, Stud };
